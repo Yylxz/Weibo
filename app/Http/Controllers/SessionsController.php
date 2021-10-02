59e7730be3a5a92d80a4ac3a98a1c36bcb3b869b
kuiperbelt <www.kuiperbelt.com>
2021-10-02 14:23:50 +0000

    登录时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -19,10 +19,12 @@ class SessionsController extends Controller
             'password'  => 'required'
         ]);
 
-        if (Auth::attempt($credentials)) {
+        if (Auth::attempt($credentials, $request->has('remember'))) {
+            // 登录成功
             session()->flash('success', '欢迎回来！');
             return redirect()->route('users.show', [Auth::user()]);
         } else {
+            // 登录失败
             session()->flash('danger', '很抱歉，您的邮箱和账号不匹配');
             return redirect()->back()->withInput();
         }
