ceb8fa2ec25efceb0dc69cd33c1d4fe96db05cc2
TreubOanna <colorabxy@outlook.com>
2021-10-02 12:03:42 +0800

    基础页面

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>@section('title', '帮助')</title>
+</head>
+<body>
+  @extends('layouts.default')
+  @section('content')
+  <h1>帮助页</h1>
+  @endsection
+</body>
+</html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>@section('title', '主页')</title>
+</head>
+<body>
+  @extends('layouts.default')
+  @section('content')
+  <h1>主页</h1>
+  @endsection
+</body>
+</html>
