f015cba17cc39c50c3d91f7889cbd36ff8afbad8
kuiperbelt <www.kuiperbelt.com>
2021-10-02 10:48:36 +0000

    用户注册表单

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,13 +6,12 @@
   </head>
   <body>
     @include('layouts._header')
-
-    <div class="container">
-      <div class="offset-md-1 col-md-10">
-        @yield('content')
-        @include('layouts._footer')
+      <div class="container">
+        <div class="offset-md-1 col-md-10">
+          @yield('content')
+          @include('layouts._footer')
+        </div>
       </div>
-    </div>
   </body>
 </html>
 
