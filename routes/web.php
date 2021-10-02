437f7ae2c35a2bc01211438d41628e3ff0b2ac40
kuiperbelt <www.kuiperbelt.com>
2021-10-02 13:57:42 +0000

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'StaticPagesController@home')->name('home');                        // 主页
-Route::get('help', 'StaticPagesController@help')->name('help');                    // 帮助
-Route::get('about', 'StaticPagesController@about')->name('about');                 // 关于
-Route::get('signup', 'UsersController@create')->name('signup');                    // 注册
+Route::get('help', 'StaticPagesController@help')->name('help');                     // 帮助
+Route::get('about', 'StaticPagesController@about')->name('about');                  // 关于
+Route::get('signup', 'UsersController@create')->name('signup');                     // 注册
 Route::resource('users', 'UsersController');
-Route::get('users/{user}', 'UsersController@show')->name('users.show');           // 隐式路由绑定 用户信息
+Route::get('users/{user}', 'UsersController@show')->name('users.show');             // 隐式路由绑定 用户信息
+Route::get('login', 'SessionsController@create')->name('login');                     // 显示登录界面
+Route::post('login', 'SessionsController@store')->name('login');                     // 创建会话
+Route::delete('logout', 'SessionsController@destroy')->name('logout');                 // 退出
+
+
