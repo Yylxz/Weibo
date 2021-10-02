9ddce3ef6b854543994559efe5ddec36a9b9d1d3
kuiperbelt <www.kuiperbelt.com>
2021-10-02 07:49:24 +0000

    添加路由注释

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'StaticPagesController@home')->name('home');
-Route::get('/help', 'StaticPagesController@help')->name('help');
-Route::get('/about', 'StaticPagesController@about')->name('about');
-Route::get('signup', 'UsersController@create')->name('signup');
+Route::get('/', 'StaticPagesController@home')->name('home');                        // 主页
+Route::get('/help', 'StaticPagesController@help')->name('help');                    // 帮助
+Route::get('/about', 'StaticPagesController@about')->name('about');                 // 关于
+Route::get('signup', 'UsersController@create')->name('signup');                     // 注册
