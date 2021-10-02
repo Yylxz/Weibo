68622a6f833d04989bdaa2c61b23cd17c742874f
kuiperbelt <www.kuiperbelt.com>
2021-10-02 15:06:52 +0000

    更改用户资料

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,10 @@ Route::get('/', 'StaticPagesController@home')->name('home');                    
 Route::get('help', 'StaticPagesController@help')->name('help');                     // 帮助
 Route::get('about', 'StaticPagesController@about')->name('about');                  // 关于
 Route::get('signup', 'UsersController@create')->name('signup');                     // 注册
-Route::resource('users', 'UsersController');
-Route::get('users/{user}', 'UsersController@show')->name('users.show');             // 隐式路由绑定 用户信息
 Route::get('login', 'SessionsController@create')->name('login');                     // 显示登录界面
 Route::post('login', 'SessionsController@store')->name('login');                     // 创建会话
-Route::delete('logout', 'SessionsController@destroy')->name('logout');                 // 退出
-
-
+Route::delete('logout', 'SessionsController@destroy')->name('logout');                // 退出
+Route::resource('users', 'UsersController');
+Route::get('users/{user}', 'UsersController@show')->name('users.show');             // 隐式路由绑定 用户信息
+Route::get('users/{user}/edit', 'UsersController@edit')->name('users.edit');        // 編輯用戶
+Route::patch('users/{user}', 'UsersController@update')->name('users.update');       // 更新
