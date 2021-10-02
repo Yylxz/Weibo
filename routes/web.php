833951aee4dc79dbb6f244a337c9e6254dce4611
kuiperbelt <www.kuiperbelt.com>
2021-10-02 09:35:13 +0000

    用户

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'StaticPagesController@home')->name('home');                        // 主页
-Route::get('/help', 'StaticPagesController@help')->name('help');                    // 帮助
-Route::get('/about', 'StaticPagesController@about')->name('about');                 // 关于
+Route::get('help', 'StaticPagesController@help')->name('help');                    // 帮助
+Route::get('about', 'StaticPagesController@about')->name('about');                 // 关于
 Route::get('signup', 'UsersController@create')->name('signup');                     // 注册
