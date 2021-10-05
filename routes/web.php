37f10286c64de4df868a67115723a4ecd0514ace
kuiperbelt <www.kuiperbelt.com>
2021-10-05 12:49:45 +0000

    修改

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,7 @@ Route::get('users/{user}', 'UsersController@show')->name('users.show');         
 Route::get('users/{user}/edit', 'UsersController@edit')->name('users.edit');        // 編輯用戶
 Route::patch('users/{user}', 'UsersController@update')->name('users.update');       // 更新
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');    // 激活账号验证
+Route::get('password/reset', 'PasswordController@showLinkRequestForm')->name('password.request');   // 填写email表单
+Route::post('password/email', 'PasswordController@sendResetLinkEmail')->name('password.email');     // 处理email表单
+Route::get('password/reset/{token}', 'PasswordController@showResetForm')->name('password.reset');   // 显示更新密码表单
+Route::post('password/reset', 'PasswordController@reset')->name('password.update');                 // 更新密码
