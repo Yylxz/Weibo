b2027232a30c6f0ef5ecca8e663a3b6aa242c410
kuiperbelt <www.kuiperbelt.com>
2021-10-07 09:53:11 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,6 +60,17 @@ class User extends Authenticatable
         return $this->hasMany(Status::class);
     }
 
+    // 获取用户的粉丝列表
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+    // 获取用户关注的列表
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
     // 头像
     public function gravatar($size = '100')
     {
@@ -70,5 +81,31 @@ class User extends Authenticatable
     public function feed()
     {
         return $this->statuses()->orderBy('created_at', 'desc');
+    }
+
+    // 关注
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+
+        $this->followings()->sync($user_ids, flase);
+    }
+
+    // 取关
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+
+        $this->followings()->detach($user_ids);
+    }
+
+    // 是否关注
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
     }
 }
